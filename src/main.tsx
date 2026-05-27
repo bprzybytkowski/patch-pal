42e28274f2e635a85a01a8f8c9f39413e7ea58a4
bprzybytkowski <bprzybytkowski@gmail.com>
2026-05-27 11:52:17 +0200

    feat: skip PostHog init when token env var is not set

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import App from './App'
 import posthog from 'posthog-js'
 import { PostHogErrorBoundary, PostHogProvider } from '@posthog/react'
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: '2026-01-30',
-})
+if (import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN) {
+  posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-01-30',
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
